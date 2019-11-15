aab6ca4c6e44872050ba14ede99ca434148e6a85
Tomas Herculano <tomasherculano@gmail.com>
2019-11-15 16:27:03 -0300

    Alerts funcionando

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,7 +3,9 @@ package gui;
 import java.io.IOException;
 import java.io.StringReader;
 
+import gui.utils.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import model.services.LexicalAnalyzer;
@@ -32,10 +34,14 @@ public class MainViewController {
 	}
 
 	public void convert(StringBuilder sb) throws IOException {
+		try {
 		LexicalAnalyzer lexical = new LexicalAnalyzer(new StringReader(sb.toString()));
 		//StringBuilder result = new StringBuilder();
 		lexical.yylex();
 		//txtAreaC.setText(lexical.yylex().toString());
 		//System.out.println(result.toString());
+		}catch(RuntimeException e) {
+			Alerts.showAlert("Sintaxe incorreta", null, "Sintaxe do pseudocódigo inválida!", AlertType.ERROR);
+		}
 	}
 }
